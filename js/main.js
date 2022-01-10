6a89e7c1e0cbcc856c23fafbec53762d728e3d2a
jakhongiralijonov <jakhongirweb@gmail.com>
2022-01-10 12:57:08 +0500

    ready

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const boy = document.querySelector('.boy')
+const coin = document.querySelector('.coin')
+
+window.addEventListener('mousedown', (e) => {
+    let x = e.clientX
+    let y = e.clientY
+    boy.style.top = `${y}px`
+    boy.style.left = `${x}px`
+
+})
+
+const bodyW = document.body.clientWidth;
+const bodyH = document.body.clientHeight;
+
+change()
+
+function change() {
+    let randomW = Math.trunc(Math.random() * bodyW);
+    let randomH = Math.trunc(Math.random() * bodyH);
+    coin.style.top = `${randomH}px`;
+    coin.style.left = `${randomW}px`;
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList[0] == 'coin') {
+        change()
+    }
+})
+
+
+
+let result = document.querySelector('.coins');
+let i = [0];
+if (boy.style.top == coin.style.top) {
+    change();
+    i.push('1')
+    result.innerHTML = `Your score ${i.length}`
+}
